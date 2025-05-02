f313821f4bef02c55a0f5537ffa7fb1b03464bea
sheldon fam <sheldonfam95@gmail.com>
2025-05-02 18:55:28 +0800

    add config setting

--- contact-form/next.config.ts
+++ contact-form/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/contact-form",
+  trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
