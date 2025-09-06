b8bd23886ef4d32d2512735c5e58023e3aaeba64
SheldonFam <sheldonfam95@gmail.com>
2025-09-07 00:04:34 +0800

    fix: remove next js config

--- contact-form/next.config.ts
+++ contact-form/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/contact-form",
-  trailingSlash: true,
-  output: "export",
 };
 
 export default nextConfig;
